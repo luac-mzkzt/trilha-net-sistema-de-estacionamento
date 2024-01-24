83c906cf419f2cdcb1760c278ec338f4548f2061
Luana Correia <155102160+luac-mzkzt@users.noreply.github.com>
2024-01-23 22:51:23 -0300

    Update Program.cs

--- DesafioFundamentos/Program.cs
+++ DesafioFundamentos/Program.cs
@@ -3,8 +3,8 @@
 // Coloca o encoding para UTF8 para exibir acentuação
 Console.OutputEncoding = System.Text.Encoding.UTF8;
 
-decimal precoInicial = 0;
-decimal precoPorHora = 0;
+decimal? precoInicial = null;
+decimal? precoPorHora = null;
 
 Console.WriteLine("Seja bem vindo ao sistema de estacionamento!\n" +
                   "Digite o preço inicial:");
